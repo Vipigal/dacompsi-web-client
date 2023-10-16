4c0c12cfaf434d5810511aa05716dee2de95fadb
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-10-16 19:20:20 -0300

    Finalizada primeira versao da home

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./config/router";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </React.StrictMode>
 );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Container from "../components/Container";
 import fotoSobreDa from "../assets/image_14.svg";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+import AvatarBox from "../components/AvatarBox";
+import collaboration from "../assets/collaboration.svg";
 
 const Home = () => {
   return (
@@ -26,8 +28,8 @@ const Home = () => {
           </span>
           <ul className="list-disc flex flex-col gap-2">
             <li className="">
-              Representar os interesses do corpo discente perante os órgãos do
-              DCC (Colegiados e Câmara);
+              Representar os interesses do corpo discente (alunos) perante os
+              órgãos do DCC (Colegiados e Câmara);
             </li>
             <li>Promover atividades integradoras entre os alunos;</li>
             <li>
@@ -45,14 +47,45 @@ const Home = () => {
       </div>
 
       <div className="bg-gradient-to-b from-red-dacompsi to-dark-red-dacompsi h-[850px]">
-        <div className="flex flex-col gap-3 text-white px-16">
-          <span className="font-bold  text-[38px]">Conheça nossa gestão</span>
+        <div className="flex flex-col gap-3 text-white px-16 ">
+          <span className="font-bold mt-6 text-[38px]">
+            Conheça nossa gestão
+          </span>
           <span>
             Esses são os membros dos Colegiados de Ciência da Computação e
             Sistemas da Informação, isto é, a ponte entre os estudantes e os
             docentes do DCC e ICEX.
           </span>
+
+          <div className="grid grid-cols-3 gap-10 mt-16 self-center gap-x-20">
+            <AvatarBox name="Chorão" />
+            <AvatarBox name="Black Alien" />
+            <AvatarBox name="Marcos" />
+            <AvatarBox name="Felipe" />
+            <AvatarBox name="João" />
+            <AvatarBox name="Jorge" />
+          </div>
         </div>
+      </div>
+
+      <div className="h-[550px] mx-40 flex justify-center gap-48">
+        <div className="flex flex-col gap-16 max-w-[558px]">
+          <span className="font-bold self-center text-[38px] min-w-max">
+            Peça ajuda a um Representante
+          </span>
+          <span>
+            Ocorreu um problema com sua matrícula? Precisa de orientação para
+            tomar uma decisão? Sem problema! Nós do DACompSI estamos aqui para
+            te ajudar! Entre em contato com nossos representantes discentes o
+            mais rápido possível e explique o seu caso! Estaremos mobilizados
+            para te ajudar.
+          </span>
+
+          <button className="rounded-full bg-red-dacompsi hover:bg-red-900 text-white w-[240px] py-2 self-center ">
+            PEDIR AJUDA
+          </button>
+        </div>
+        <img src={collaboration} />
       </div>
     </Container>
   );
